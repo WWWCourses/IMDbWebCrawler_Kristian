1d313102de8d9e066db295f33d85c2959c13cd73
Kristian <kris.kunchev@gmail.com>
2022-06-30 16:53:31 +0300

    Add new functions

--- Crawler_app.py
+++ Crawler_app.py
@@ -11,10 +11,8 @@ class Crawler:
         self.save_html(html, 'imdb.html')
         scraper = Scraper(html)
         urls = scraper.get_into_movie_title()
-        urls = [base_url + url for url in urls]
+        #urls = [url for url in urls]
         print(urls)
-        best_urls = scraper.get_film_links()
-        print(best_urls)
 
     def get_url(self):
         r = requests.get(self.url)
