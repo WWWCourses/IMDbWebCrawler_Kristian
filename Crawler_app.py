83052c53d44437501171e6afb0639c40ee3313cf
WWWCourses <ivawebcourses@gmail.com>
2022-07-08 17:41:01 +0300

    done: get movies urls, which rating is bigger than 8

--- Crawler_app.py
+++ Crawler_app.py
@@ -1,23 +1,50 @@
+from re import M
 import requests
 from Scrapper import Scraper
 
 
 class Crawler:
-    def __init__(self, url):
+    def __init__(self, url, base_url):
         self.url = url
+        self.base_url = base_url
 
     def start(self):
-        html = self.get_url()
+        # get main page html
+        html = self.get_html(self.url)
         self.save_html(html, 'imdb.html')
-        scraper = Scraper(html)
-        urls = scraper.get_into_movie_title()
-        #urls = [url for url in urls]
-        print(urls)
 
-    def get_url(self):
-        r = requests.get(self.url)
+        # get movies urls, which rating is bigger than 8
+        self.movies_urls = self.get_movies_urls(min_rating=8)
+        print(self.movies_urls)
+
+        # TODO: from each url in movies_url get movie info
+
+    def get_movies_urls(self, min_rating):
+        html = self.get_html(self.url)
+        scraper = Scraper(html)
+
+        movies_urls = scraper.get_movies_urls(min_rating)
+        # prepend base_url to each element in movies urls:
+        movies_urls = [self.base_url+url for url in movies_urls]
+        return movies_urls
+
+        # TODO:
+
+
+    def get_html(self, url):
+        try:
+            r = requests.get(url)
+        except Exception as e:
+            print(f'Can not get url: {url}: {str(e)}!')
+            exit(-1)
+
+        r.encoding="utf-8"
+
+        # if we have repsonse => get and return html content
         if r.ok:
-            return r.text
+            html = r.text
+
+        return html
 
     def save_html(self, content, filename):
         with open(filename, 'w', encoding='utf-8') as f:
@@ -25,6 +52,7 @@ class Crawler:
 
 
 if __name__ == '__main__':
-    base_url = 'https://www.imdb.com'
-    craw = Crawler('https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm')
+    craw = Crawler( url='https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm',
+                    base_url = 'https://www.imdb.com'
+    )
     craw.start()
--- Scrapper.py
+++ Scrapper.py
@@ -14,6 +14,27 @@ class Scraper:
     #             rate = float(x.get_text())
     #             return rate
 
+    def get_movies_urls(self,min_rating):
+        # get all movies info rows from 'tbody.lister-list'
+        rows = self.soup.select('tbody.lister-list tr')
+
+        # the list into which relevat movies urls will be stored:
+        movies_urls = []
+
+        for row in rows:
+            # get IMDB rating value - the content of strong element which is in td.imdbRating
+            rating_element = row.select_one('td.imdbRating strong')
+            if rating_element:
+                rating = float(rating_element.get_text())
+
+                if rating>min_rating:
+                    # get movie url:
+                    movie_url = row.select_one('td.titleColumn a')['href']
+                    movies_urls.append(movie_url)
+
+        return movies_urls
+
+
     def get_into_movie_title(self):
         urls = list()
         base_url = 'https://www.imdb.com'
